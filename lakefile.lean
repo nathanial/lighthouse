9b4fd3dcd67f8aa1809f5ac04dfdd925b3bf0adc
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-31 01:22:17 -0800

    Update terminus and ledger to v0.0.2

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,8 @@ open Lake DSL
 package lighthouse where
   precompileModules := true
 
-require terminus from git "https://github.com/nathanial/terminus" @ "v0.0.1"
-require ledger from git "https://github.com/nathanial/ledger" @ "v0.0.1"
+require terminus from git "https://github.com/nathanial/terminus" @ "v0.0.2"
+require ledger from git "https://github.com/nathanial/ledger" @ "v0.0.2"
 require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
 
 @[default_target]
